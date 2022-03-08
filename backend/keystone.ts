28e54b7f1eb76fa0522ac2dfc540d4f58a01040a
roluochKe <rasholuoch@gmail.com>
2022-03-08 15:36:57 +0300

    adding project schema

---
+++ backend/keystone.ts
@@ -0,0 +1,52 @@
+import { createAuth } from '@keystone-next/auth'
+import { config, createSchema  } from '@keystone-next/keystone/schema'
+import 'dotenv/config'
+import { withItemData, statelessSessions}  from '@keystone-next/keystone/session'
+
+import { User } from './schemas/User'
+import { Product } from './schemas/Product'
+import { ProductImage } from './schemas/ProductImage'
+
+const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost:27017/shopper-keystone'
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, // How long they stay signed in?
+  secret: process.env.COOKIE_SECRET
+}
+
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password']
+  }
+})
+
+export default withAuth(config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true
+    }
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL
+  },
+  lists: createSchema({
+    User,
+    Product,
+    ProductImage,
+  }),
+  ui: {
+
+    isAccessAllowed: ({session}) => {
+      return !!session?.data
+    }
+  },
+  session: withItemData(statelessSessions(sessionConfig), {
+    User: `id`
+  })
+}))
+
---
+++ backend/schemas/ProductImage.ts
@@ -0,0 +1,27 @@
+import 'dotenv/config'
+import { list } from "@keystone-next/keystone/schema";
+import { cloudinaryImage } from "@keystone-next/cloudinary";
+import { relationship, text } from "@keystone-next/fields";
+
+export const cloudinary = {
+  cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+  apiKey: process.env.CLOUDINARY_KEY,
+  apiSecret: process.env.CLOUDINARY_SECRET,
+  folder: 'shopper'
+}
+
+export const ProductImage = list({
+  fields: {
+    image: cloudinaryImage({
+      cloudinary,
+      label: 'source'
+    }),
+    altText: text(),
+    product: relationship({ref: "Product.photo"})
+  },
+  ui: {
+    listView: {
+      initialColumns: ['image', 'altText', 'product']
+    }
+  }
+})
